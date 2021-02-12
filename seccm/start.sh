e29de81b653a61d644d4a5041e416f63500617ad
Jose Ignacio Jerez Rodriguez <jjerezro@redhat.com>
2021-02-12 13:57:13 +0100

    Added a file reference to start.sh
    Signed-off-by: Jose Ignacio Jerez Rodriguez <jjerezro@redhat.com>

--- seccm/start.sh
+++ seccm/start.sh
@@ -2,22 +2,28 @@
 #Variables
 WAIT_DEF=4
 COUNT_DEF=0
+FILE_TEXT="/opt/repo/text.txt"
 
 if [ -z $WAIT ]
 then
   echo "Variable WAIT not defined, using default value of $WAIT_DEF"
   WAIT=$WAIT_DEF
+else
+  echo "Variable WAIT is defined with value $WAIT"
 fi
 
 if [ -z $COUNT ]
 then
   echo "Variable COUNT not defined, using default value of $COUNT_DEF"
   COUNT=$COUNT_DEF
+else
+  echo "Variable COUNT is defined with values $COUNT"
 fi
 
 while true
 do
   echo "Waiting $COUNT"
+  if [ -f $FILE_TEXT ]; then cat $FILE_TEXT; fi
   (( COUNT++ ))
   sleep $WAIT
 done
