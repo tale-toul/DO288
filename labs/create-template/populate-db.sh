dd44880c5f8879b77dba4239a643e04f9701f1c1
Jose Ignacio Jerez Rodriguez <jjerezro@redhat.com>
2021-02-21 12:37:41 +0100

    Use different path and password to populate the database
    Signed-off-by: Jose Ignacio Jerez Rodriguez <jjerezro@redhat.com>

--- labs/create-template/populate-db.sh
+++ labs/create-template/populate-db.sh
@@ -6,8 +6,8 @@ oc port-forward $(basename ${pod}) 30306:3306 &
 tunnel=$!
 sleep 3
 echo 'Initializing the database...'
-mysql -h127.0.0.1 -P30306 -uquoteapp -pmypass quotesdb \
-    < ~/DO288/labs/create-template/quote.sql
+mysql -h127.0.0.1 -P30306 -uquoteapp -pSuperSecretPass
+    < DO288/labs/create-template/quote.sql
 sleep 3
 echo 'Terminating tunnel...'
 kill ${tunnel}
